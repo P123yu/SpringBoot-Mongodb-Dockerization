bde778a29bdd4c71d00b606ebae7964745c85eeb
Piyush Kumar Singh <96623633+P123yu@users.noreply.github.com>
2024-05-04 16:48:02 +0530

    Update UserRepository.java

--- src/main/java/com/mongo/repository/UserRepository.java
+++ src/main/java/com/mongo/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.mongo.repository;
 import com.mongo.model.UserModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository extends MongoRepository<UserModel,Long> {
+public interface UserRepository extends MongoRepository<UserModel,String> {
 }
